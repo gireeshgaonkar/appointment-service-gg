60c99817caa7b47ffc92d62bf996b6e07059819c
Gireesh <gireeshgg48@outlook.com>
2025-07-19 15:02:46 +0530

    updated main.tf and variable files and providers

--- variable.tf
+++ variable.tf
@@ -17,6 +17,27 @@ variable "region" {
   description = "The azure region to deploy the infrastructure"
   default     = "us-east-1"
 }
+
+
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  default     = "${env}-vnet"
+}
+
+variable "address_prefixes" {
+  description = "The address prefixes for the virtual network"
+  default     = ["10.0.0.0/16"]
+}
+variable "public_subnet_prefix" {
+  description = "The address prefix for the public subnet"
+  default     = ["10.0.1.0/24"]
+}
+
+variable "private_subnet_prefix" {
+  description = "The address prefix for the private subnet"
+  default     = ["10.0.2.0/24"]
+}
+
 variable "resource_group_name" {
   description = "The name of the resource group"
   default     = "${env}_ResourceGroup"
@@ -46,3 +67,17 @@ variable "app_storage_name" {
   default     = "${env}StorageAccount"
 }
 
+variable "dns_name" {
+  description = "The DNS name for the function app"
+  default     = "${env}-function-app"
+}
+
+variable "dns_zone_name" {
+  description = "The DNS zone name"
+  default     = "${env}.${app_service_name}.com"
+}
+
+variable "function_app_dns_name" {
+  description = "The DNS name for the function app"
+  default     = "${env}-function-app.${dns_zone_name}"
+} 
